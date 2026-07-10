00a7340fb1cd876944e813dc88f2cf327478d17f
Satyansh-Rawat-131 <lapras1812@gmail.com>
2026-07-11 04:35:52 +0530

    Improved formatting of the output

--- services/parser.py
+++ services/parser.py
@@ -2,16 +2,38 @@ import fitz
 
 def extract_pdf_text(uploaded_file):
 
+    """
+    Returns:
+        {
+            "name" = "<filename>"
+            "pages" =
+            [
+                {
+                    "page" = 1,
+                    "text" = "..."
+                },
+            ...
+            ]
+        }
+    """
+
     pdf = fitz.open(
         stream=uploaded_file.read(),
         filetype="pdf"
     )
+    pages = []
 
     text = ""
 
-    for page in pdf:
+    for page_num , page  in enumerate (pdf , start = 1):
         text += page.get_text()
+        pages.append({
+            "page" : page_num,
+            "text" : text
+        })
 
     pdf.close()
 
-    return text
+    return {
+        "name" : uploaded_file.name , "pages" : pages
+    }
